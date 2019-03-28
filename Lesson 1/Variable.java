4a8904a55419655dfaf4f3f9845dd4b8e8d9ae77
Anastasia Mkrtchyan <nastasya1011@gmail.com>
2019-03-28 22:44:12 +0300

    отформитировала код

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,16 +1,15 @@
 public class Variable {
-	public static void main(String[] args){
-
+	public static void main(String[] args) {
 		byte inch = 13;
 		short ram = 8;
 		int cacheL3 = 3;
-		long  cacheL2 = 256;
+		long cacheL2 = 256;
 		float processor = 0.5f;
 		double hdd = 120.11;
 		char graphicRam = 1536;
 		boolean isGrey = true;
 
-	System.out.println(""+inch+ram+cacheL3+cacheL2+processor+hdd+graphicRam+isGrey);
+	System.out.println("" + inch + ram + cacheL3 + cacheL2 + processor + hdd + graphicRam + isGrey);
 
 
 	}
