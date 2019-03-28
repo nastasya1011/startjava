23b21461814d962c93b18bb99d1a796821823f53
Anastasia Mkrtchyan <nastasya1011@gmail.com>
2019-03-28 21:10:22 +0300

    Добавила класс Variable

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+	public static void main(String[] args){
+
+		byte inch = 13;
+		short ram = 8;
+		int cacheL3 = 3;
+		long  cacheL2 = 256;
+		float processor = 0.5f;
+		double hdd = 120.11;
+		char graphicRam = 1536;
+		boolean isGrey = true;
+
+	System.out.println(""+inch+ram+cacheL3+cacheL2+processor+hdd+graphicRam+isGrey);
+
+
+	}
+}
