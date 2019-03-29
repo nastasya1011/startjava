7d9207d3f9a4afedeefa32abd975e637631693ae
Anastasia Mkrtchyan <nastasya1011@gmail.com>
2019-03-29 14:36:32 +0300

    выставила translate_tabs_to_spaces:true

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,14 +1,14 @@
 public class Variable {
-	public static void main(String[] args) {
-		byte inch = 13;
-		short ram = 8;
-		int cacheL3 = 3;
-		long cacheL2 = 256;
-		float processor = 0.5f;
-		double hdd = 120.11;
-		char graphicRam = 1536;
-		boolean isGrey = true;
+    public static void main(String[] args) {
+        byte inch = 13;
+        short ram = 8;
+        int cacheL3 = 3;
+        long cacheL2 = 256;
+        float processor = 0.5f;
+        double hdd = 120.11;
+        char graphicRam = 1536;
+        boolean isGrey = true;
 
-	System.out.println("" + inch + ram + cacheL3 + cacheL2 + processor + hdd + graphicRam + isGrey);
-	}
+    System.out.println("" + inch + ram + cacheL3 + cacheL2 + processor + hdd + graphicRam + isGrey);
+    }
 }
